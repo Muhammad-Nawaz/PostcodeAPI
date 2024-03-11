272d22888dd23bf16876be8decbfc977547f22ed
Muhammad Nawaz <nawazm621@outlook.com>
2024-03-11 16:41:54 +0000

    added 2 get methods for postcode API clas

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import json
 import requests
 
-response = requests.get('https://api.postcodes.io/postcodes/UB24PG')
-print(response.json())
+
+class PostcodeApi:
+    def __init__(self):
+        pass
+
+    def get_postcode_info(self, postcode):
+        response = requests.get(f'https://api.postcodes.io/postcodes/{postcode}')
+        if response.status_code == 200:
+            return print(response.json())
+        else:
+            return None
+
+    def get_postcode_info_random(self):
+        response = requests.get('https://api.postcodes.io/random/postcodes')
+        if response.status_code == 200:
+            return print(response.json())
+        else:
+            return None
+
+
+postcode = PostcodeApi()
+postcode.get_postcode_info('UB24PG')
+postcode.get_postcode_info_random()
