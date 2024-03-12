8a3d21ca96cb632b14465e9f63340bf64b175511
Muhammad Nawaz <nawazm621@outlook.com>
2024-03-12 11:26:34 +0000

    updated create_postcode method to get_bulk_lookup instead

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import requests
 
 
@@ -27,10 +28,10 @@ class PostcodeApi:
             print(f"Error: {e}")
 
 
-    def create_postcode(self, postcode_data):
+    def get_bulk_lookup(self, postcode_data):
         try:
             response = requests.post('https://api.postcodes.io/postcodes/', json=postcode_data)
-            if response.status_code == 201:
+            if response.status_code == 200:
                 return response.json()
             else:
                 return None
@@ -47,39 +48,8 @@ random_postcode = postcode.get_postcode_info_random()
 print(random_postcode)
 
 postcode_data = {
-    "postcode": "SW1A1AA",
-    "quality": 1,
-    "eastings": 530047,
-    "northings": 179951,
-    "country": "England",
-    "nhs_ha": "London",
-    "longitude": -0.127695,
-    "latitude": 51.507322,
-    "european_electoral_region": "London",
-    "primary_care_trust": "Westminster",
-    "region": "London",
-    "lsoa": "Westminster 018A",
-    "msoa": "Westminster 018",
-    "incode": "1AA",
-    "outcode": "SW1A",
-    "parliamentary_constituency": "Cities of London and Westminster",
-    "admin_district": "Westminster",
-    "parish": "Westminster, unparished area",
-    "admin_county": None,
-    "admin_ward": "St James's",
-    "ccg": "NHS Central London (Westminster)",
-    "nuts": "Westminster",
-    "codes": {
-        "admin_district": "E09000033",
-        "admin_county": "E99999999",
-        "admin_ward": "E05000644",
-        "parish": "E43000236",
-        "parliamentary_constituency": "E14000639",
-        "ccg": "E38000032",
-        "ccg_id": "02D",
-        "ced": "E99999999",
-        "nuts": "UKI32"
-    }
+  "postcodes" : ["PR3 0SG", "M45 6GN", "EX165BL"]
 }
-create_postcode = postcode.create_postcode(postcode_data)
-print(create_postcode)
+
+bulk_lookup = postcode.get_bulk_lookup(postcode_data)
+print(bulk_lookup)
